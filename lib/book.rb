7526243997ee56a2a254b492e56802ca94760e07
Brayarn Ogeda <brayarn.oduor@student.moringaschool.com>
2022-10-22 14:52:16 +0300

    completed putting it all together lab

--- lib/book.rb
+++ lib/book.rb
@@ -1,2 +1,18 @@
+class Book
+  attr_accessor :title, :author, :page_count, :genre
 
+  def initialize(title)
+    @title = title
+  end
 
+  def turn_page
+   p "Flipping the page...wow, you read fast!"
+  end
+end
+
+book = Book.new("And Then There Were None")
+p book.title
+p book.author = "Agatha Christie"
+p book.page_count = 272
+p book.genre = "Mystery"
+book.turn_page
--- lib/shoe.rb
+++ lib/shoe.rb
@@ -1,1 +1,20 @@
 # Make your shoe class here!
+class Shoe
+  attr_accessor :brand, :color, :size, :material, :condition
+
+  def initialize(brand)
+   @brand = brand
+  end
+
+  def cobble
+    p "Your shoe is as good as new!"
+    @condition = "new"
+  end
+end
+
+shoe = Shoe.new("Nike")
+p shoe.brand
+p shoe.color = "black"
+p shoe.size = 8
+p shoe.material = "leather"
+p shoe.condition = "old"
